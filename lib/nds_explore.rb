6f25fa815a35939c6f770a50d3807e1a9912e9ff
Jake Pino <jakepino@yahoo.com>
2020-02-14 07:52:20 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,23 +10,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  
-  row_index = 0
-while row_index < directors_database.length do
-  column_index = 0
-  while column_index < directors_database[row_index].length do
-    inner_len = directors_database[row_index][column_index].length
-    inner_index = 0
-    while inner_index < inner_len do
-      # Explanation!
-      # vm[row][column][spinner]
-      # spinner is full of Hashes with keys :price and :name
-      print directors_database[row_index][column_index][inner_index][:title]
-      inner_index += 1
-    end
-    column_index += 1
-  end
-  row_index += 1
-end
-  
+  print directors_database
 end
