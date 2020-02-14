f0528703acfeb4a5b6b31ae7e6c143b76a28c299
Jake Pino <jakepino@yahoo.com>
2020-02-14 07:43:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   
   i = 0
   while i < directors_database.length do 
-    puts directors_database[0][i][:title]
+    puts directors_database[:movies][i]
     i += 1
   end
 end
